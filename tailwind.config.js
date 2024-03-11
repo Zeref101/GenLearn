4fd510b3dd096fe9059f97a8a6b20e02647cba0d
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-11 23:53:59 +0530

    Add react-router-dom dependency and remove unnecessary code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ import { nextui } from "@nextui-org/react";
 
 /** @type {import('tailwindcss').Config} */
 export const content = [
-  // ...
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
   "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 ];
 export const theme = {
